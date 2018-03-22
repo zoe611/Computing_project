76442228858cea55bf08f2b866a86c8d102578b1
junwenz <junwen08331@gmail.com>
2018-03-22 16:19:51 +1100

    update front framework

---
+++ dev/frontend/vue-front/src/components/Search/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="search">
+        <h1>this is search</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Home',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ dev/frontend/vue-front/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/components/Home/Home'
+import Search from '@/components/Search/Search'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    }
+  ]
+})
