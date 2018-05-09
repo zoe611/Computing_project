e139718496d988555e7a21b505085283f739b1d9
zoe611 <1994zzy777@gmail.com>
2018-05-09 18:33:51 +1000

    update visual

--- dev/frontend/vue-front/src/components/Profile/Profile.vue
+++ dev/frontend/vue-front/src/components/Profile/Profile.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <h1>This is profile</h1>
   </div>
 </template>
 
@@ -13,19 +12,23 @@ export default {
       nodes: [
         {
           name: 'Alice',
-          id: 2407
+          id: 2407,
+          color: '#D24D25'
         },
         {
           name: 'Bob',
-          id: 2247
+          id: 2247,
+          color: '#FFCD34'
         },
         {
           name: 'Carol',
-          id: 4567
+          id: 4567,
+          color: '#FFCD34'
         },
         {
           name: 'Dog',
-          id: 5678
+          id: 5678,
+          color: '#FFCD34'
         }
       ],
       links: [
@@ -45,76 +48,94 @@ export default {
           value: 4
         }
       ],
-      msg: 'Welcome to Your Vue.js App',
-      svg: null,
-      graph: null,
-      simulation: null,
       settings: {
         strokeColor: '#29B5FF',
         width: 100,
         svgWigth: 960,
         svgHeight: 600
-      }
+      },
+      msg: 'Welcome to Your Vue.js App',
+      svg: null,
+      graph: null,
+      simulation: null,
+      circle: null
     }
   },
-  mounted: function () {
-    var that = this
-    that.svg = d3.select('.home')
-      .append('svg')
-      .attr('width', 960)
-      .attr('height', 600)
-    that.simulation = d3.forceSimulation(that.nodes)
-      .force('link', d3.forceLink(that.links).distance(100).strength(0.1))
-      .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter(that.settings.svgWigth / 2, that.settings.svgHeight / 2))
-    that.nodes = d3.select('svg').append('g')
-      .attr('class', 'nodes')
-      .selectAll('circle')
-      .data(that.nodes)
-      .enter().append('circle')
-      .attr('r', 20)
-      .attr('fill', 'red')
-      .on('click', function (d) {
-        console.log(d.name)
+  methods: {
+    showSvg () {
+      console.log('rerer')
+      var that = this
+      that.svg = d3.select('.relation')
+        .append('svg')
+        .attr('width', 960)
+        .attr('height', 600)
+      that.simulation = d3.forceSimulation(that.nodes)
+        .force('link', d3.forceLink(that.links).distance(100).strength(0.1))
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter(that.settings.svgWigth / 2, that.settings.svgHeight / 2))
+      var links = d3.select('svg').append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(that.links)
+        .enter().append('line')
+        .attr('stroke', '#0080A4')
+        .attr('stroke-width', function (d) { return d.value })
+      var nodes = d3.select('svg').append('g')
+        .attr('class', 'nodes')
+        .selectAll('g')
+        .data(that.nodes)
+        .enter().append('g')
+        .on('mouseover', function (d, i) {
+          d3.select(this).append('text').text(function (d) {
+            return d.name
+          })
+            .attr('x', 6)
+            .attr('y', 3)
+          d3.select(this).append('title')
+            .text(function (d) { return d.name })
+        })
+        .on('mouseout', function (d, i) {
+          d3.select(this).select('title').remove()
+          d3.select(this).select('text').remove()
+        })
+      that.circle = nodes
+        .append('circle')
+        .attr('r', 20)
+        .data(that.nodes)
+        .attr('fill', function (d) {
+          return d.color
+        })
+        .on('click', function (d) {
+          console.log(d.name)
+        })
+        .call(d3.drag()
+          .on('start', function dragstarted (d) {
+            if (!d3.event.active) that.simulation.alphaTarget(0.3).restart()
+            d.fx = d.x
+            d.fy = d.y
+          })
+          .on('drag', function dragged (d) {
+            d.fx = d3.event.x
+            d.fy = d3.event.y
+          })
+          .on('end', function dragended (d) {
+            if (!d3.event.active) that.simulation.alphaTarget(0)
+            d.fx = null
+            d.fy = null
+          })
+        )
+      that.simulation.on('tick', function ticked () {
+        nodes
+          .attr('transform', function (d) {
+            return 'translate(' + d.x + ',' + d.y + ')'
+          })
+        links
+          .attr('x1', function (d) { return d.source.x })
+          .attr('y1', function (d) { return d.source.y })
+          .attr('x2', function (d) { return d.target.x })
+          .attr('y2', function (d) { return d.target.y })
       })
-      .call(d3.drag()
-        .on('start', function dragstarted (d) {
-          if (!d3.event.active) that.simulation.alphaTarget(0.3).restart()
-          d.fx = d.x
-          d.fy = d.y
-        })
-        .on('drag', function dragged (d) {
-          d.fx = d3.event.x
-          d.fy = d3.event.y
-        })
-        .on('end', function dragended (d) {
-          if (!d3.event.active) that.simulation.alphaTarget(0)
-          d.fx = null
-          d.fy = null
-        })
-      )
-    console.log(that.nodes)
-    console.log('testtest')
-    that.links = d3.select('svg').append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(that.links)
-      .enter().append('line')
-      .attr('stroke', 'blue')
-      .attr('stroke-width', function (d) { return d.value })
-    console.log(that.links)
-    console.log('testtest')
-    that.simulation.on('tick', function ticked () {
-      that.links
-        .attr('x1', function (d) { return d.source.x })
-        .attr('y1', function (d) { return d.source.y })
-        .attr('x2', function (d) { return d.target.x })
-        .attr('y2', function (d) { return d.target.y })
-      that.nodes
-        .attr('cx', function (d) { return d.x })
-        .attr('cy', function (d) { return d.y })
-    })
-    console.log('testtest')
+    }
   }
 }
 </script>
--- dev/frontend/vue-front/src/components/Search/Search.vue
+++ dev/frontend/vue-front/src/components/Search/Search.vue
@@ -93,6 +93,7 @@
         </div>
       </div>
     </div>
+    <div class="relation" v-if="showrelation()"></div>
     <!--    filter by date  and sort modified -->
     <!--<div class = "search_filter" v-if="show()">-->
     <div style="height:10px;width:100%;"></div>
@@ -169,6 +170,45 @@ export default {
   },
   data () {
     return {
+      nodes: [
+        {
+          name: 'Alice',
+          id: 2407,
+          color: '#D24D25'
+        },
+        {
+          name: 'Bob',
+          id: 2247,
+          color: '#FFCD34'
+        },
+        {
+          name: 'Carol',
+          id: 4567,
+          color: '#FFCD34'
+        },
+        {
+          name: 'Dog',
+          id: 5678,
+          color: '#FFCD34'
+        }
+      ],
+      links: [
+        {
+          source: 0,
+          target: 1,
+          value: 2
+        },
+        {
+          source: 0,
+          target: 2,
+          value: 3
+        },
+        {
+          source: 0,
+          target: 3,
+          value: 4
+        }
+      ],
       api: 'http://43.240.98.137/test2.php',
       request: {
         method: '',
@@ -179,6 +219,16 @@ export default {
         sort: 'relative',
         author_id: ''
       },
+      settings: {
+        strokeColor: '#29B5FF',
+        width: 100,
+        svgWigth: 400,
+        svgHeight: 400
+      },
+      svg: null,
+      graph: null,
+      simulation: null,
+      circle: null,
       loading: false,
       result: {},
       isRecent: true,
@@ -258,6 +308,90 @@ export default {
       var end = this.$refs.endTime.value
       var filter = 'BETWEEN ' + start + ' AND ' + end
       this.search_filter(filter)
+    },
+    showrelation () {
+      // if (this.result.bar) {
+      //   this.showSvg()
+      //   return true
+      // } else {
+      //   return false
+      // }
+      this.showSvg()
+      return true
+    },
+    showSvg () {
+      var that = this
+      var svg = d3.select('.relation')
+        .append('svg')
+        .attr('class', 'test')
+        .attr('width', that.settings.svgWigth)
+        .attr('height', that.settings.svgHeight)
+      var simulation = d3.forceSimulation(that.nodes)
+        .force('link', d3.forceLink(that.links).distance(100).strength(0.1))
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter(that.settings.svgWigth / 2, that.settings.svgHeight / 2))
+      var links = d3.select('.test').append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(that.links)
+        .enter().append('line')
+        .attr('stroke', '#0080A4')
+        .attr('stroke-width', function (d) { return d.value })
+      var nodes = d3.select('.test').append('g')
+        .attr('class', 'nodes')
+        .selectAll('g')
+        .data(that.nodes)
+        .enter().append('g')
+        .on('mouseover', function (d, i) {
+          d3.select(this).append('text').text(function (d) {
+            return d.name
+          })
+            .attr('x', 6)
+            .attr('y', 3)
+          d3.select(this).append('title')
+            .text(function (d) { return d.name })
+        })
+        .on('mouseout', function (d, i) {
+          d3.select(this).select('title').remove()
+          d3.select(this).select('text').remove()
+        })
+      var circle = nodes
+        .append('circle')
+        .attr('r', 20)
+        .data(that.nodes)
+        .attr('fill', function (d) {
+          return d.color
+        })
+        .on('click', function (d) {
+          console.log(d.name)
+        })
+        .call(d3.drag()
+          .on('start', function dragstarted (d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+            d.fx = d.x
+            d.fy = d.y
+          })
+          .on('drag', function dragged (d) {
+            d.fx = d3.event.x
+            d.fy = d3.event.y
+          })
+          .on('end', function dragended (d) {
+            if (!d3.event.active) simulation.alphaTarget(0)
+            d.fx = null
+            d.fy = null
+          })
+        )
+      simulation.on('tick', function ticked () {
+        nodes
+          .attr('transform', function (d) {
+            return 'translate(' + d.x + ',' + d.y + ')'
+          })
+        links
+          .attr('x1', function (d) { return d.source.x })
+          .attr('y1', function (d) { return d.source.y })
+          .attr('x2', function (d) { return d.target.x })
+          .attr('y2', function (d) { return d.target.y })
+      })
     }
   }
 }
